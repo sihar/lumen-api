0ac957cdbaceee3e08c05e3fb1eb8aae075d9770
Sihar William <simbolonsihar@gmail.com>
2025-02-11 13:51:15 +0700

    generate users table and setting jwt

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,26 +8,28 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User extends Model implements AuthenticatableContract, JWTSubject
 {
     use Authenticatable, Authorizable, HasFactory;
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
 
     /**
-     * The attributes that are mass assignable.
+     * Return a key value array, containing any custom claims to be added to the JWT.
      *
-     * @var string[]
+     * @return array
      */
-    protected $fillable = [
-        'name', 'email',
-    ];
-
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var string[]
-     */
-    protected $hidden = [
-        'password',
-    ];
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+Route::group([
+    'prefix' => 'api'
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('user-profile', 'AuthController@me');
+});
